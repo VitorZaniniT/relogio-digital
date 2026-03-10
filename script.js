f79892e23a57c835f492d2f3272d4d2be6ef3b2c
VitorZaniniT <vitorzanini321@gmail.com>
2026-03-10 17:53:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function atualizarRelogio(){
+
+let agora = new Date()
+
+let horas = agora.getHours()
+let minutos = agora.getMinutes()
+let segundos = agora.getSeconds()
+
+horas = horas < 10 ? "0" + horas : horas
+minutos = minutos < 10 ? "0" + minutos : minutos
+segundos = segundos < 10 ? "0" + segundos : segundos
+
+document.getElementById("hora").textContent =
+horas + ":" + minutos + ":" + segundos
+
+}
+
+setInterval(atualizarRelogio,1000)
+
+atualizarRelogio()
